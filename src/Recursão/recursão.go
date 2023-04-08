19d14b85315c8220d5727d4338bd4acba021fd14
Arlyjr <arly.lcj@gmail.com>
2023-04-07 22:43:32 -0300

    Recursão

---
+++ src/Recursão/recursão.go
@@ -0,0 +1,16 @@
+//Recursão é a capacidade de uma função chamar ela mesma
+
+package main
+
+import "fmt"
+
+func fatorial(x int) int {
+	if x == 0 {
+		return 1
+	}
+	return x * fatorial(x-1)
+}
+
+func main() {
+	fmt.Println(fatorial(9))
+}
